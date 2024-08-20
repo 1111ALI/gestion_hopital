9cd95ce3a3b83d24d692363670f1bd9c2a3bc4e8
Yann Kwadjio <kwadjionry@gmail.com>
2024-08-20 03:56:24 +0100

    first-commit

---
+++ src/main/java/com/gestion_hopital/GHopitalApplication.java
@@ -0,0 +1,13 @@
+package com.gestion_hopital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GHopitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GHopitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestion_hopital/GHopitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion_hopital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GHopitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
